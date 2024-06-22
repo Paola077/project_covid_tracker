9df1cb5a0424707b6681f049fc8d101bed223c1e
MabelZar <mabel.zar.esp@gmail.com>
2024-06-23 01:27:31 +0200

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import Home from "./pages/Home"
 import "./components/home/nav/nav.css"
 import { Route, Routes } from "react-router-dom"
 import Tracker1 from "./pages/Tracker1.jsx"
-import TotalCases from "./components/tracker1/TotalCases.jsx"
-import TotalConfirmed from "./components/tracker1/TotalConfirmed.jsx"
 
 
 const App = () =>{
@@ -14,8 +12,7 @@ const App = () =>{
         <Route path='/' element={<Home />} />
         <Route path='/tracker1' element={<Tracker1 />} />
       </Routes>
-      <TotalCases></TotalCases>
-      <TotalConfirmed></TotalConfirmed>
+  
 
     
        </>
