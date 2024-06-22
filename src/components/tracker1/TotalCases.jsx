47180e38eceabd848cd15c43f2767aab27a95cdd
MabelZar <mabel.zar.esp@gmail.com>
2024-06-23 01:15:49 +0200

    total confirmed

--- src/components/tracker1/TotalCases.jsx
+++ src/components/tracker1/TotalCases.jsx
@@ -1,27 +1,27 @@
-import {useEffect, useState} from 'react';
-import axios from 'axios';
-import "./totalCases.css"
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./totalCases.css";
 
-
-const TotalCases=()=>{
-    const [country, setCountry]= useState(null);
-    const [totalCases, setCases]= useState(null);
-    useEffect(()=>{
-        axios.get('https://disease.sh/v3/covid-19/countries/usa')
-        .then(res=>{
-            console.log(res.data.country);
-            setCountry(res.data.country);
-            setCases(res.data.cases)
-        })
-    })
-return(
-<>
-<div className='totalCases'> 
-    <p>Total Cases:{country}</p>
-    <p className='txt'>{totalCases}</p>
-    <img src="src/assets/img/covid-defult.svg" className='img'/>
-</div>
-</>
-)
-}
-export default TotalCases
+const TotalCases = () => {
+  const [totalCases, setCases] = useState(null);
+  useEffect(() => {
+    axios.get("https://disease.sh/v3/covid-19/countries/usa").then((res) => {
+      console.log(res.data.cases);
+      setCases(res.data.cases);
+    });
+  });
+  return (
+    <>
+      <div className="tarjetaContent">
+        <div className="totalCases">
+          <p>Total Cases:</p>
+          <p className="txt">{totalCases}</p>
+        </div>
+        <div className="imgContent">
+          <img src="src/assets/img/covid-defult.svg" className="img" />
+        </div>
+      </div>
+    </>
+  );
+};
+export default TotalCases;
