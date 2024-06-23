43dcb6fa98584c90182cb93bc6bfb261533fc2a8
MabelZar <mabel.zar.esp@gmail.com>
2024-06-23 02:21:23 +0200

    correction app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import Home from "./pages/Home"
 import "./components/home/nav/nav.css"
 import { Route, Routes } from "react-router-dom"
-import Tracker1 from "./pages/Tracker1.jsx"
+import Dashboard from "./pages/Dashboard.jsx"
 
 
 const App = () =>{
     return(
-      
-    <>
-   <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/tracker1' element={<Tracker1 />} />
-      </Routes>
-    
-       </>
+        <>
+    <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+        </Routes>
+        
+        </>
     )
 }
 
