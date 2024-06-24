728318547bb7e7087178eac3adc9c0c3bf0dd98d
paolaperdomo <paolaperdomo079@gmail.com>
2024-06-24 21:02:20 +0200

    Implementacion inicial

--- src/pages/Tracker3.jsx
+++ src/pages/Tracker3.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import TitleComp from '../components/TitlesTrakers/TitleComp'
+import CountriesTable from '../components/tracker3/CountriesTable'
 
 const Tracker3 = () => {
     return (
         <div>
         <TitleComp title='Top 10 Country wise Covid-19 Upgrades - Tiles'></TitleComp>
+        <CountriesTable></CountriesTable>
         </div>
     )
 }
