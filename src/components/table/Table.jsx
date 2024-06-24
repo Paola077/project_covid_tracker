845717d77da7dc5cc99642649246c7e70594fed8
Cris <crispr.zde@gmail.com>
2024-06-24 15:50:16 +0200

    Tabla T2 creada con DataTables jQuery y Boostrap

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,149 +1,67 @@
-import DataTable from "react-data-table-component";
-import { useEffect, useState } from "react";
+import React, { useEffect, useRef } from 'react';
+import axios from 'axios';
+import $ from 'jquery';
+import 'datatables.net-bs5';
+import 'datatables.net-responsive';
+import 'datatables.net-responsive-bs5';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css';
+import 'datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css';
 import './table.css';
-import axios from "axios";
 
 const Table = () => {
-    const [data, setData] = useState([]);
-    const [records, setRecords] = useState([]);
-
+    const tableRef = useRef();
 
     useEffect(() => {
-        axios.get('https://disease.sh/v3/covid-19/countries')
-            .then(response => {
-                setData(response.data);
-                setRecords(response.data)
-            })
-            .catch(error => console.error('Error fetching data: ', error));
-    }, []);
-    
-
-    const columns = [
-        {
-            name: 'Flag',
-            selector: row => <img src={row.countryInfo.flag} alt={`Flag of ${row.country}` } style={{ width: '40px' }}/>,
-            sortable: false,
-        },
-        {
-            name: 'Country',
-            selector: row => row.country,
-            sortable: true,
-        },
-        {
-            name: 'Cases',
-            selector: row => row.cases,
-            sortable: true,
-        },
-        {
-            name: 'New Cases',
-            selector: row => row.todayCases,
-            sortable: true,
-        },
-        {
-            name: 'Deaths',
-            selector: row => row.deaths,
-            sortable: true,
-        },
-        {
-            name: 'New Deaths',
-            selector: row => row.todayDeaths,
-            sortable: true,
-        },
-        {
-            name: 'Recovered',
-            selector: row => row.recovered,
-            sortable: true,
-        },
-        {
-            name: 'Active',
-            selector: row => row.active,
-            sortable: true,
-        },
-        {
-            name: 'Critical',
-            selector: row => row.critical,
-            sortable: true,
-        },
-        {
-            name: 'Tested',
-            selector: row => row.tests,
-            sortable: true,
-        },
-    ]
-    
-    const handleChange = (e) => {
-        const filteredRecords = data.filter(record =>{
-            return record.country.toLowerCase().includes(e.target.value.toLowerCase())
+            axios
+        .get('https://disease.sh/v3/covid-19/countries')
+        .then((response) => {
+            if ($.fn.dataTable.isDataTable(tableRef.current)) {
+                $(tableRef.current).DataTable().destroy();
+            }
+            $(tableRef.current).DataTable({
+            data: response.data,
+            columns: [
+                {
+                title: 'Flag',
+                data: 'countryInfo.flag',
+                render: function (data) {
+                    return `<img src="${data}" alt="Flag" style="width: 2.50rem;" />`;
+                },
+                },
+                { title: 'Country', data: 'country' },
+                { title: 'Cases', data: 'cases' },
+                { title: 'New Cases', data: 'todayCases' },
+                { title: 'Deaths', data: 'deaths' },
+                { title: 'New Deaths', data: 'todayDeaths' },
+                { title: 'Recovered', data: 'recovered' },
+                { title: 'Active', data: 'active' },
+                { title: 'Critical', data: 'critical' },
+                { title: 'Tested', data: 'tests' },
+            ],
+            
+            responsive: true,
+            language: {
+                paginate: {
+                previous: 'Previous',
+                next: 'Next',
+                },
+                length: 'Show ',
+                info: 'Showing _START_ to _END_ of _TOTAL_ entries',
+                lengthMenu: 'Show _MENU_ entries',
+            },
+            
+            });
         })
-        setRecords(filteredRecords)
-    };
-    
+        
+        .catch((error) => console.error('Error fetching data: ', error));
+    }, []);
 
     return (
         <div className="tableT2">
-            <div className="controlsT2">
-                <label className="searchT2">
-                Search:
-                <input type="text" onChange={handleChange} />
-                </label>
-            </div>
-            <DataTable
-                columns={columns}
-                data={records}
-                pagination
-                paginationPerPage={10}
-                paginationRowsPerPageOptions={[10, 20, 30, 50]}
-                fixedHeader
-                customStyles={customStyles}
-            />
-            </div>
-        );
-    };
-    
-    const customStyles = {
-        headCells: {
-            style: {
-            color: '#12125C',
-            backgroundColor: 'transparent',
-            fontWeight: 'bold',
-            },
-        },
-        rows: {
-            style: {
-            color: '#12125C',
-            backgroundColor: '#F8F9FE',
-            '&:nth-of-type(odd)': {
-                backgroundColor: '#F8F9FE',
-            },
-            '&:nth-of-type(even)': {
-                backgroundColor: '#E8E9FE',
-            },
-            },
-        },
-        pagination: {
-            style: {
-            color: '#333',
-            fontSize: '0.69rem',
-            minHeight: '2.5rem',
-            backgroundColor: 'transparent',
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            },
-            pageButtonsStyle: {
-            borderRadius: '0.15rem',
-            padding: '0.5rem',
-            margin: '0.13rem',
-            cursor: 'pointer',
-            transition: '0.4s',
-            color: 'red',
-            backgroundColor: '#fff',
-            '&:hover': {
-                backgroundColor: '#12125C',
-                fill:'white'
-            },
-            },
-        },
-    };
-    
-    export default Table;
+        <table ref={tableRef} className="table table-striped" style={{ width: '100%' }}></table>
+        </div>
+    );
+};
+
+export default Table;
