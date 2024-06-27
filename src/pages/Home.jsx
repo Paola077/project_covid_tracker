c98d26d4e92497ec65bea3bd1a592ac069485de8
CarolBV <carolina.botella.valero@gmail.com>
2024-06-27 19:30:50 +0200

    agregar wash

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@
 import Nav from "../components/home/nav/Nav.jsx"
 import Footer from '../components/home/footer/Footer.jsx'
 import News from '../components/home/news/News.jsx'
+import Wash from "../components/home/wash/Wash.jsx"
 
 const Home = () => {
   return (
     <>
       <Nav/>
+      <Wash/>
       <News/>
       <Footer/>
     </>
