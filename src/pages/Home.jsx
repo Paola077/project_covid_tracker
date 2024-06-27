0173f9d5fa002470e782c387bdf5ae17b0d434fc
MabelZar <mabel.zar.esp@gmail.com>
2024-06-27 18:15:37 +0200

    change symptom

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,10 @@
 // import React from 'react'
 import Nav from "../components/home/nav/Nav.jsx"
-import Symptom from "../components/home/Symptom/Symptom.jsx"
 
 const Home = () => {
   return (
     <>
     <Nav/>
-    <Symptom/>
     </>
   )
 }
