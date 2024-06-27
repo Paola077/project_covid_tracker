a8338e78d0dff74fcb1123190fa15b523e0e84ec
MabelZar <mabel.zar.esp@gmail.com>
2024-06-27 18:13:15 +0200

    add symptom component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 // import React from 'react'
 import Nav from "../components/home/nav/Nav.jsx"
+import Symptom from "../components/home/Symptom/Symptom.jsx"
 
 const Home = () => {
   return (
+    <>
     <Nav/>
+    <Symptom/>
+    </>
   )
 }
 
