439ee224f3ba2e5baa0f9dd807dfa7579efb1987
Cris <crispr.zde@gmail.com>
2024-06-27 17:15:05 +0200

    Parte superior de Home montada

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 // import React from 'react'
-import Nav from "../components/home/nav/Nav.jsx"
 import Footer from '../components/home/footer/Footer.jsx'
+import HomeTop from '../components/home/homeTop/HomeTop.jsx'
 import News from '../components/home/news/News.jsx'
+
+
 
 const Home = () => {
   return (
     <>
-      <Nav/>
+      <HomeTop/>
       <News/>
       <Footer/>
     </>
