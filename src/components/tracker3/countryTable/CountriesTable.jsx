d594716a0a1f5643692c8fa390fba4847563b067
paolaperdomo <paolaperdomo079@gmail.com>
2024-06-28 09:46:26 +0200

    Mergeando main

--- src/components/tracker3/countryTable/CountriesTable.jsx
+++ src/components/tracker3/countryTable/CountriesTable.jsx
@@ -1,66 +1,61 @@
 import React, { useState, useEffect} from "react";
 import DataTable from "react-data-table-component";
-import axios from "axios";
 import { columnsTopCases, columnsTodayCases, columnsTodayDeaths, columnsTopActive, columnsTodayrecovered, columnsTodayDeathsSecond } from "./dataColumns";
 import './countriesTable.css'
-import { dataFake } from "../cards/dataFake";
+import { getDataByCountry } from "../../services/apiService";
 
  const CountriesTable = () => {
-   /* const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+    const [data, setData] = useState([]);
    
     useEffect(() => {
-        const getData = async () => {
-            try {
-                const response = await axios.get("https://disease.sh/v3/covid-19/countries/");
-                const sortedData = response.data.sort((a, b) => b.active - a.active); 
-                console.log("Sorted data:", sortedData);
-                const topTenData = sortedData.slice(0, 10);
-                console.log("Top 10 data:", topTenData);
-                setData(topTenData);
-            } catch (error) {
-                setError(error);
-            } finally {
-                setLoading(false);
-            }
+        const fetchData = async () => {
+          try {
+            const result = await getDataByCountry();
+            setData(result);
+            setData(topTenData);
+          } catch (error) {
+            console.error('Error fetching global data: ', error);
+          }
         };
-        getData();
-    }, []);
+    
+        fetchData();
+      }, []);
+    
+      if (data === null) {
+        return <>Loading</>;
+      }
 
-    if (loading) return <div>Cargando...</div>;
-    if (error) return <div>Error: {error.message}</div>; */
     return (
         <>
             <div className="containerTable"> 
                 <DataTable
                 columns={columnsTopCases}
-                data={dataFake}
+                data={data}
                 pagination={false}
                 />
                 <DataTable
                 columns={columnsTodayCases}
-                data={dataFake}
+                data={data}
                 pagination={false}
                 />
                 <DataTable
                 columns={columnsTodayDeaths}
-                data={dataFake}
+                data={data}
                 pagination={false}
                 />
                 <DataTable
                 columns={columnsTodayDeathsSecond}
-                data={dataFake}
+                data={data}
                 pagination={false}
                 />
                 <DataTable
                 columns={columnsTopActive}
-                data={dataFake}
+                data={data}
                 pagination={false}
                 />
                 <DataTable
                 columns={columnsTodayrecovered}
-                data={dataFake}
+                data={data}
                 pagination={false}
                 />
             </div>
