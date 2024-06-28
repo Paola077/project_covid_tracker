8f5318c3993fc7685645ec16555e5a86dafc9153
CarolBV <carolina.botella.valero@gmail.com>
2024-06-28 15:06:35 +0200

    arreglar css trackers

--- src/components/CardCases/CardCases.jsx
+++ src/components/CardCases/CardCases.jsx
@@ -6,7 +6,7 @@ const CardCases = ({ cardsBgStyle, cardText, smallTextValue, cardDataStyle, glob
     <>
       <div className={`cardCasesContainer ${cardsBgStyle}`}>
         <div className="cardCasesTitle">
-          <h5>{cardText}</h5>
+          <h5 className="cardTextTracker0">{cardText}</h5>
         </div>
         <div className="flex updatedCasesContainer">
           <p className="todayCasesText">{smallTextValue}</p>
