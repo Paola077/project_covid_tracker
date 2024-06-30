0c503d07f44be50a1fcf79a761c21fe039979f6b
Erieltxu <ld.aldecoa@gmail.com>
2024-06-30 22:35:39 +0200

    logowhite en carpeta test

--- src/components/test/LogoWhite.test.jsx
+++ src/components/test/LogoWhite.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import LogoWhite from "./LogoWhite"; 
+import LogoWhite from "../logoWhite/LogoWhite"; 
 import { test } from "vitest";
 
 test('LogoWhite component', () => {
